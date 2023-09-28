b18ba1896aa0cac9ed66dd3619d1f3875eba7917
Gmoody2000 <127790461+Gmoody2000@users.noreply.github.com>
2023-09-28 18:37:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+// Initialize the previousFoods array
+let previousFoods = [];
+
 // Define the searchFood function
 async function searchFood() {
     const foodNameInput = document.getElementById("food-name");
     const foodName = encodeURIComponent(foodNameInput.value.trim());
     const searchResults = document.getElementById("search-results");
+    const previousResults = document.getElementById("previous-results");
+    const totalResults = document.getElementById("total-results");
 
     // Show loading message while waiting for API response
     searchResults.innerHTML = '<p>Loading...</p>';
@@ -32,16 +37,53 @@ async function searchFood() {
             let html = '';
             result.forEach((food) => {
                 console.log('Nutritional Information for', food.name, ':', food); // Log each food item
+
+                // Log the details to the previousFoods array
+                previousFoods.push({
+                    name: food.name,
+                    calories: food.calories,
+                    protein: food.protein_g,
+                    carbs: food.carbohydrates_total_g,
+                    fat: food.fat_total_g,
+                    servingSize: food.serving_size_g,
+                });
+
+                // Add the details to the UI
                 html += `
-                    <h3>Nutritional Information for ${food.name}</h3>
-                    <p>Calories: ${food.calories ? food.calories : 'N/A'} kcal</p>
-                    <p>Protein_g: ${food.protein_g !== undefined ? food.protein_g : 'N/A'}g</p>
-                    <p>Carbohydrates_total_g: ${food.carbohydrates_total_g !== undefined ? food.carbohydrates_total_g : 'N/A'}g</p>
-                    <p>Fat_total_g: ${food.fat_total_g !== undefined ? food.fat_total_g : 'N/A'}g</p>
-                    <p>serving_size_g: ${food.serving_size_g !== undefined ? food.serving_size_g : 'N/A'}g</p>
-                    <hr />`; // Add a horizontal line between entries
+                <h3>Nutritional Information for ${food.name}</h3>
+                <p>Calories: ${food.calories ? food.calories : 'N/A'} kcal</p>
+                <p>Protein: ${food.protein_g !== undefined ? food.protein_g : 'N/A'}g</p>
+                <p>Carbohydrates: ${food.carbohydrates_total_g !== undefined ? food.carbohydrates_total_g : 'N/A'}g</p>
+                <p>Fat: ${food.fat_total_g !== undefined ? food.fat_total_g : 'N/A'}g</p>
+                <p>Serving Size: ${food.serving_size_g !== undefined ? food.serving_size_g : 'N/A'}g</p>
+                <hr />`; // Add a horizontal line between entries
             });
+
+            // Append the details to the searchResults container
             searchResults.innerHTML = html;
+
+            // Log the previous foods
+            console.log('Previous Foods:', previousFoods);
+
+            // Calculate and display the totals
+            const totalCalories = previousFoods.reduce((acc, food) => acc + (food.calories || 0), 0);
+            const totalProteins = previousFoods.reduce((acc, food) => acc + (food.protein || 0), 0);
+            const totalCarbs = previousFoods.reduce((acc, food) => acc + (food.carbs || 0), 0);
+            const totalFats = previousFoods.reduce((acc, food) => acc + (food.fat || 0), 0);
+
+            const totalHtml = `
+            <h3>Total Nutritional Information</h3>
+            <p>Total Calories: ${totalCalories} kcal</p>
+            <p>Total Protein: ${totalProteins}g</p>
+            <p>Total Carbohydrates: ${totalCarbs}g</p>
+            <p>Total Fat: ${totalFats}g</p>
+            `;
+
+            // Append the details to the totalResults container
+            totalResults.innerHTML = totalHtml;
+
+            // Append the details to the previousResults container
+            previousResults.innerHTML += html;
         } else {
             // Handle the case where the API request is successful but the data is not as expected
             console.error('API request successful, but data is undefined or not as expected.');
